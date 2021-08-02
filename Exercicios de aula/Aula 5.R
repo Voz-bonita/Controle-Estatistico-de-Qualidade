b797f8d34500722c3a65eaca1748303e68976dbf
Jose Vitor <jose.vitor.sem.c@gmail.com>
2021-08-02 11:34:44 -0300

    Aula 5 completa

--- Exercicios de aula/Aula 5.R
+++ Exercicios de aula/Aula 5.R
@@ -7,3 +7,14 @@ LC <- x_sum/m/n
 LIC <- LC - 3*sqrt(LC*(1-LC)/n)
 LSC <- LC + 3*sqrt(LC*(1-LC)/n)
 
+### Hipotese de melhoria
+
+LC.old <- 0.2150
+LC.new <- 0.1108
+
+n1 <- 28*50
+n2 <- 24*50
+phat <- (LC.old*n1 + n2*LC.new)/(n1+n2)
+
+Z0 <- (LC.old-LC.new)/sqrt(phat*(1-phat)*(1/n1 + 1/n2))
+Z0 > qnorm(0.95) ## Melhoria aceita
