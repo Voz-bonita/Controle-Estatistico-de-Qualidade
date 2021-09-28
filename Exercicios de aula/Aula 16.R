5422605525497daa304b086653ba3b93036cca13
Jose Vitor <jose.vitor.sem.c@gmail.com>
2021-09-28 17:37:17 -0300

    Aula 16, revisada

---
+++ Exercicios de aula/Aula 16.R
@@ -0,0 +1,44 @@
+pacman::p_load("purrr", "stringr", "dplyr")
+
+n <- 2
+k <- 3
+
+sign_vector <- c(-1, -1, -1, 1,	1,	1,	-1,
+                1,	-1,	-1,	-1,	-1,	1,	1,
+                -1,	1,	-1,	-1,	1,	-1,	1,
+                1,	1,	-1,	1,	-1,	-1,	-1,
+                -1,	-1,	1,	1,	-1,	-1,	1,
+                1,	-1,	1,	-1,	1,	-1,	-1,
+                -1,	1,	1,	-1,	-1,	1,	-1,
+                1,	1,	1,	1,	1,	1,	1,
+                -1,	-1,	-1,	1,	1,	1,	-1,
+                1,	-1,	-1,	-1,	-1,	1,	1,
+                -1,	1,	-1,	-1,	1,	-1,	1,
+                1,	1,	-1,	1,	-1,	-1,	-1,
+                -1,	-1,	1,	1,	-1,	-1,	1,
+                1,	-1,	1,	-1,	1,	-1,	-1,
+                -1,	1,	1,	-1,	-1,	1,	-1,
+                1,	1,	1,	1,	1,	1,	1)
+
+# Forma de tabela para facilitar os calculos
+matrix_signs <- as_tibble(t(matrix(sign_vector, nrow = 7, ncol = 16)))
+
+exp1 <- c(9,10,9,12,11,10,10,16)
+exp2 <- c(7,12,11,15,10,13,8,14)
+experimento <- c(exp1, exp2)
+
+Constrastes <- map_dbl(matrix_signs, ~sum(.x * experimento))
+names(Constrastes) <- c("A", "B", "C", "AB", "AC", "BC", "ABC")
+
+Efeitos <- Constrastes / (2^(k-1) * n)
+
+SQs <- Constrastes^2 / (n * 2^k)
+SQmodelo <- sum(SQs)
+
+mu_hat <- mean(experimento)
+
+SQtotal <- sum((experimento - mu_hat)^2)
+SQres <- SQtotal - SQmodelo
+
+Coeficientes <- Efeitos / 2
+
